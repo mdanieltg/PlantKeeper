3ae3ee43497753b3d3645a1c4adf3d5a4b48c032
Daniel Medrano Huerta <daniel@quicktortoise.com>
2024-06-22 07:37:06 -0600

    Update KeeperList component

--- PlantKeeperWebApp/src/app/keepers/keeper-list/keeper-list.component.ts
+++ PlantKeeperWebApp/src/app/keepers/keeper-list/keeper-list.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Keeper } from "../keeper";
+import { NgForOf } from "@angular/common";
 
 @Component({
   selector: 'app-keeper-list',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './keeper-list.component.html',
   styleUrl: './keeper-list.component.css'
 })
 export class KeeperListComponent {
-
+  keepers: Keeper[] = [
+    { id: '', name: 'Daniel' },
+    { id: '', name: 'Diego' },
+    { id: '', name: 'Saul' },
+    { id: '', name: 'Mo' },
+  ]
 }
