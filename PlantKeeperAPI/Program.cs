867a7385fafb298fafdc40ddf283abef89e1cde9
Daniel Medrano Huerta <daniel@quicktortoise.com>
2024-06-22 07:28:50 -0600

    Add missing CORS configuration

--- PlantKeeperAPI/Program.cs
+++ PlantKeeperAPI/Program.cs
@@ -7,6 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers(options => options.ReturnHttpNotAcceptable = true);
 builder.Services.AddDatabase(builder.Configuration, builder.Environment);
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("devenv", policyBuilder => policyBuilder
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowAnyOrigin());
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -19,7 +26,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    app.UseCors();
+    app.UseCors("devenv");
 }
 else
 {
