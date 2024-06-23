0bb9216feec9bd161f611dc8824a519a8bff889b
Daniel Medrano Huerta <daniel@quicktortoise.com>
2024-06-23 14:23:40 -0600

    Add page not found component

--- PlantKeeperWebApp/src/app/app.routes.ts
+++ PlantKeeperWebApp/src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
-import { StartComponent } from "./start/start.component";
-import { PlantListComponent } from "./plants/plant-list/plant-list.component";
-import { PlantDetailsComponent } from "./plants/plant-details/plant-details.component";
-import { KeeperListComponent } from "./keepers/keeper-list/keeper-list.component";
 import { KeeperDetailsComponent } from "./keepers/keeper-details/keeper-details.component";
-import { WateringMethodListComponent } from "./watering-methods/watering-method-list/watering-method-list.component";
+import { KeeperListComponent } from "./keepers/keeper-list/keeper-list.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { PlantDetailsComponent } from "./plants/plant-details/plant-details.component";
+import { PlantListComponent } from "./plants/plant-list/plant-list.component";
+import { StartComponent } from "./start/start.component";
 import {
   WateringMethodDetailsComponent
 } from "./watering-methods/watering-method-details/watering-method-details.component";
+import { WateringMethodListComponent } from "./watering-methods/watering-method-list/watering-method-list.component";
 
 export const routes: Routes = [
   { path: 'keepers', component: KeeperListComponent, pathMatch: 'full' },
@@ -17,4 +18,5 @@ export const routes: Routes = [
   { path: 'watering-methods', component: WateringMethodListComponent, pathMatch: 'full' },
   { path: 'watering-methods/:id', component: WateringMethodDetailsComponent, pathMatch: 'full' },
   { path: '', component: StartComponent, pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
 ];
